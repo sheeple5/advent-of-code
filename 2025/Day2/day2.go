014db4a715d51313c0da489439641a9f24ada709
Carson-Reed12 <carsonreed97@gmail.com>
2025-12-02 08:55:25 -0600

    Adding 2025 Day2

---
+++ 2025/Day2/day2.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getFileInput(fileName string) string {
+	dat, err := os.ReadFile(fileName)
+	check(err)
+
+	return string(dat)
+}
+
+func processRange(idRange string, part string) int {
+	firstID, _ := strconv.Atoi(strings.Split(idRange, "-")[0])
+	secondID, _ := strconv.Atoi(strings.TrimSpace(strings.Split(idRange, "-")[1]))
+
+	invalidSum := 0
+	for id := firstID; id <= secondID; id++ {
+		if part == "PART 1" {
+			if invalidID(id) {
+				invalidSum += id
+			}
+		} else if part == "PART 2" {
+			if bruteReptitions(id) {
+				invalidSum += id
+			}
+		}
+	}
+
+	return invalidSum
+}
+
+func invalidID(id int) bool {
+	stringID := strconv.Itoa(id)
+
+	if len(stringID)%2 != 0 {
+		return false
+	} else {
+		idHalfLen := len(stringID) / 2
+		idFirstHalf := stringID[:idHalfLen]
+		idSecondHalf := stringID[idHalfLen:]
+
+		if idFirstHalf == idSecondHalf {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+func bruteReptitions(id int) bool {
+	stringID := strconv.Itoa(id)
+	var sequence string
+
+	for _, char := range stringID {
+		sequence += string(char)
+		sequenceLen := len(sequence)
+		if sequenceLen > len(stringID)/2 {
+			return false
+		}
+
+		invalid := true
+		for i := 0; i < len(stringID); i += sequenceLen {
+			if i+sequenceLen > len(stringID) {
+				invalid = false
+				break
+			}
+			if stringID[i:i+sequenceLen] != sequence {
+				invalid = false
+				break
+			}
+		}
+
+		if invalid {
+			return true
+		}
+
+	}
+	return false
+}
+
+func main() {
+	// SETUP
+	idRanges := strings.Split(getFileInput("day2_input.txt"), ",")
+
+	// PART 1
+	totalInvalidSum := 0
+	for _, idRange := range idRanges {
+		totalInvalidSum += processRange(idRange, "PART 1")
+	}
+	fmt.Printf("PART 1 INVALID ID SUM: %d\n", totalInvalidSum)
+
+	// PART 2
+	totalInvalidSum = 0
+	for _, idRange := range idRanges {
+		totalInvalidSum += processRange(idRange, "PART 2")
+	}
+	fmt.Printf("PART 2 INVALID ID SUM: %d\n", totalInvalidSum)
+}
