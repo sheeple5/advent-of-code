8ab6b4de96f0d1f94ad9796cf6bb7ade50127773
Carson Reed <carsonreed97@gmail.com>
2025-12-06 16:17:00 -0600

    Cleaning up 2025 Day6

--- 2025/Day6/day6.go
+++ 2025/Day6/day6.go
@@ -26,52 +26,34 @@ func copySlice(origSlice []string) []string {
 	return newSlice
 }
 
-func totalCalculations(calculations [][]string) int {
-	numCalculations := len(calculations[0])
-	numOperators := len(calculations)
-
-	summedCalculations := 0
-	for i := range numCalculations {
-		operator := calculations[numOperators-1][i]
-		var calculationResult int
-
-		switch operator {
-		case "+":
-			calculationResult = 0
-		case "*":
-			calculationResult = 1
-		}
-
-		for j := range numOperators - 1 {
-			intVal, _ := strconv.Atoi(calculations[j][i])
-			switch operator {
-			case "+":
-				calculationResult += intVal
-			case "*":
-				calculationResult *= intVal
-			}
-		}
-		summedCalculations += calculationResult
-	}
-	return summedCalculations
-}
-
-func checkBlankColumn(stringCalculations []string, index int) bool {
+func formatCalculations(stringCalculations []string) [][]string {
+	var calculations [][]string
 	for _, line := range stringCalculations {
-		if string(line[index]) != " " {
-			return false
-		}
+		calculations = append(calculations, strings.Fields(line))
 	}
-	return true
+
+	var formattedCalculations [][]string
+	var formattedCalculation []string
+	numOperators := len(calculations)
+	numCalculations := len(calculations[0])
+	for i := range numCalculations {
+		for j := range numOperators {
+			formattedCalculation = append(formattedCalculation, calculations[j][i])
+		}
+		formattedCalculations = append(formattedCalculations, copySlice(formattedCalculation))
+		formattedCalculation = formattedCalculation[:0]
+	}
+	return formattedCalculations
 }
 
 func constructColumnCalculations(stringCalculations []string) [][]string {
 	numOperators := len(stringCalculations)
+	numCalculations := len(stringCalculations[0])
+	operator := string(stringCalculations[numOperators-1][0])
 	var constructedCalculations [][]string
 	var constructedValues []string
-	operator := string(stringCalculations[numOperators-1][0])
 
-	for i := 0; i < len(stringCalculations[0]); i++ {
+	for i := range numCalculations {
 		if checkBlankColumn(stringCalculations, i) {
 			constructedValues = append(constructedValues, operator)
 			constructedCalculations = append(constructedCalculations, copySlice(constructedValues))
@@ -82,7 +64,7 @@ func constructColumnCalculations(stringCalculations []string) [][]string {
 		}
 
 		newVal := ""
-		for j := 0; j < numOperators-1; j++ {
+		for j := range numOperators - 1 {
 			char := string(stringCalculations[j][i])
 			if char != " " {
 				newVal += char
@@ -94,6 +76,15 @@ func constructColumnCalculations(stringCalculations []string) [][]string {
 	constructedValues = append(constructedValues, operator)
 	constructedCalculations = append(constructedCalculations, copySlice(constructedValues))
 	return constructedCalculations
+}
+
+func checkBlankColumn(stringCalculations []string, index int) bool {
+	for _, line := range stringCalculations {
+		if string(line[index]) != " " {
+			return false
+		}
+	}
+	return true
 }
 
 func calculateResult(calculation []string) int {
@@ -124,18 +115,18 @@ func main() {
 	// SETUP
 	stringCalculations := strings.Split(getFileInput("day6_input.txt"), "\n")
 	stringCalculations = stringCalculations[:len(stringCalculations)-1]
-	var calculations [][]string
-
-	for _, line := range stringCalculations {
-		calculations = append(calculations, strings.Fields(line))
-	}
 
 	// PART 1
-	fmt.Printf("PART 1 TOTAL CALCULATIONS: %d\n", totalCalculations(calculations))
+	formattedCalculations := formatCalculations(stringCalculations)
+	totalValue := 0
+	for _, calculation := range formattedCalculations {
+		totalValue += calculateResult(calculation)
+	}
+	fmt.Printf("PART 1 TOTAL CALCULATIONS: %d\n", totalValue)
 
 	// PART 2
 	constructedCalculations := constructColumnCalculations(stringCalculations)
-	totalValue := 0
+	totalValue = 0
 	for _, calculation := range constructedCalculations {
 		totalValue += calculateResult(calculation)
 	}
